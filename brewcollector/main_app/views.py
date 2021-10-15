0517a0bf2210db05264a29cc46518da938549386
alexmcdermid <alexander.mcdermid@gmail.com>
2021-10-15 13:14:15 -0700

    update data schema and homepage

--- brewcollector/main_app/views.py
+++ brewcollector/main_app/views.py
@@ -6,19 +6,25 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>its brewtime')
+  return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 class Brew:
-    def __init__(self, name):
+    def __init__(self, name, beerType, brewery, ibu, alcoholPercent, price):
         self.name = name
+        self.beerType = beerType
+        self.brewery = brewery
+        self.ibu = ibu
+        self.alcoholPercent = alcoholPercent
+        self.price = price
+
 
 brews = [
-    Brew('Pale Ale'),
-    Brew('IPA'),
-    Brew('Summer Ale')
+    Brew('Vancouver Pale Ale','Pale Ale', 'Vancouver Brewing', '20', '5%','2$'),
+    Brew('Vancouver IPA','IPA', 'Vancouver Brewing', '80', '8%','4$'),
+    Brew('Vancouver Pilsner','Pilsner', 'Vancouver Brewing', '40', '4%','3$')
 ]
 
 def brews_index(request):
