a4cf9b7c0dcdec3dac03e66b1be277443c838f5e
alexmcdermid <alexander.mcdermid@gmail.com>
2021-10-19 09:09:33 -0700

    finish finch collector lab part 3

--- brewcollector/main_app/urls.py
+++ brewcollector/main_app/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('brews/', views.brews_index, name='index'),
-    path('brews/<int:brew_id>/', views.brews_detail, name='detail')
+    path('brews/<int:brew_id>/', views.brews_detail, name='detail'),
+    path('brews/new', views.brews_new),
+    path('brews_submit/', views.brews_create),
+    path('brews/<int:brew_id>/delete/', views.brews_delete),
+    path('brews/<int:brew_id>/edit/', views.brews_edit), 
+    path('brews/<int:brew_id>/submit_update_form/', views.brews_update) 
 ]
--- brewcollector/main_app/views.py
+++ brewcollector/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Brew
 
 # Create your views here.
@@ -20,3 +20,37 @@ def brews_index(request):
 def brews_detail(request, brew_id):
     brew = Brew.objects.get(id=brew_id)
     return render(request, 'brews/detail.html', {'brew': brew})
+
+def brews_new(request):
+    return render(request,'brews/new.html', {})
+
+def brews_create(request):
+    brew = Brew.objects.create(
+        name = request.POST['name'],
+        beerType = request.POST['beerType'],
+        brewery = request.POST['brewery'],
+        ibu = request.POST['ibu'],
+        alcoholPercent = request.POST['alcoholPercent'],
+        price = request.POST['price']
+    )
+    return redirect(f'/brews/{brew.id}')
+
+def brews_delete(request, brew_id):
+    brew = Brew.objects.get(id=brew_id)
+    brew.delete()
+    return redirect('/brews')
+
+def brews_edit(request, brew_id):
+    brew = Brew.objects.get(id=brew_id)
+    return render(request, 'brews/edit.html', {'brew':brew})
+
+def brews_update(request, brew_id):
+    brew = Brew.objects.get(id=brew_id)
+    brew.name = request.POST['name']
+    brew.beerType = request.POST['beerType']
+    brew.brewery = request.POST['brewery']
+    brew.ibu = request.POST['ibu']
+    brew.alcoholPercent = request.POST['alcoholPercent']
+    brew.price = request.POST['price']
+    brew.save()
+    return redirect(f'/brews/{brew.id}')
